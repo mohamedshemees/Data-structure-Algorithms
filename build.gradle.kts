66f105ace45d58682155df7b361d444f027171dd
mohamedshemees <mohamedessampd@gmail.com>
2025-04-27 08:11:22 +0300

    add: implement Jacoco for test coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,6 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit library
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     implementation ("io.reactivex.rxjava3:rxjava:3.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
