8499e9fd5f312f2353a79cc5fe4108a23909b2e5
mohamedshemees <mohamedessampd@gmail.com>
2025-04-27 07:22:18 +0300

    add: test coverage workflow for pull requests

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit library
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("io.reactivex.rxjava3:rxjava:3.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
