0c0847cba7a5dfc1434a7586e5eda4f5c2cc20c7
mohamedshemees <72915905+mohamedshemees@users.noreply.github.com>
2025-04-27 09:06:25 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,32 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    jacoco
+}
+
+jacoco {
+    toolVersion = "0.8.10"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "1.0".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
 }
 
 group = "org.example"
@@ -13,10 +40,14 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit library
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
     implementation ("io.reactivex.rxjava3:rxjava:3.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+
+    implementation("io.insert-koin:koin-core:4.0.2")
+    testImplementation ("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation ("io.mockk:mockk:1.13.10")
+    testImplementation ("com.google.truth:truth:1.4.2")
 }
 
 tasks.test {
