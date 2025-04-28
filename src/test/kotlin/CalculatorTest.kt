863aaba40351fa837b6db08d2eca58bd7ae81b6a
mohamedshemees <72915905+mohamedshemees@users.noreply.github.com>
2025-04-28 16:50:03 +0300

    Update CalculatorTest.kt

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -14,10 +14,21 @@ class CalculatorTest {
         val sum = calculator.add(2, 3)
         assertEquals(5, sum)
     }
+    @Test
+    fun `test addition2`() {
+        val sum = calculator.add(6, 3)
+        assertEquals(5, sum)
+    }
 
     @Test
     fun `test subtraction`() {
         val difference = calculator.subtract(5, 3)
+        assertEquals(2, difference)
+    }
+
+    @Test
+    fun `test subtraction2`() {
+        val difference = calculator.subtract(1, 7)
         assertEquals(2, difference)
     }
 }
