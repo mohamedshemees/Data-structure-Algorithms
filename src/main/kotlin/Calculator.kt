576bbc7abbeb0c887439c67c81a3804cdef79813
mohamedshemees <72915905+mohamedshemees@users.noreply.github.com>
2025-04-28 16:50:14 +0300

    Update Calculator.kt

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -2,10 +2,20 @@ package com.example.com.example
 
 class Calculator {
     fun add(a: Int, b: Int): Int {
+        if (a>b){
+            println("Test")
+        }else{
+            println("covering branches")
+        }
         return a + b
     }
 
     fun subtract(a: Int, b: Int): Int {
+        if (a>b){
+            println("Test")
+        }else{
+            println("covering branches")
+        }
         return a - b
     }
 }
